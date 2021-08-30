a2c6590e2bdca09b291312b999fec4557908940d
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-08-29 18:05:57 -0700

    even

--- personal/eoo/main.py
+++ personal/eoo/main.py
@@ -4,7 +4,10 @@ try:
     high = input("Enter an upper bound: ")
     high = int(high)
 
-    for i in range(low, high, 4):
+    if low % 2 == 0:
+        low += 1
+
+    for i in range(low, high + 1, 4):
         print(f"{i}, ", end="")
 except:
     print("Invalid number.")
