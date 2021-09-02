384ae6f301d3474997e40ba7122b870af6a1082a
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-09-02 11:09:33 -0700

    working bot

--- personal/eoo/discord-bot/bot.py
+++ personal/eoo/discord-bot/bot.py
@@ -1,6 +1,8 @@
 import discord
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 client = discord.Client()
 
 
@@ -11,13 +13,37 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
+    msg = message.content
+
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+    if msg.startswith("$eoo"):
+        args = msg.split(" ")
+        try:
+            low = int(args[1])
+            high = int(args[2])
+        except:
+            await message.channel.send("You can only give me valid integers...")
+            return
+
+        if low % 2 == 0:
+            low += 1
+
+        nums = gen_eoo(low, high)
+        await message.channel.send(
+            "EOO numbers between " + str(low) + " & " + str(high) + ":    " + nums
+        )
 
 
-# print(os.environ)
-# print(os.environ.get("TOKEN"))
+def gen_eoo(low, high):
+    nums = ""
+    for i in range(low, high + 1, 4):
+        nums += str(i) + ", "
+    nums = nums.strip()
+    nums = nums.rstrip(",")
+    return nums
+
+
 client.run(os.getenv("TOKEN"))
