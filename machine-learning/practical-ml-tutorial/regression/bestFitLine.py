6443ef9d55d636b864970297f87d04937b839153
Curtis Mitchell <curtisjmitchell1@gmail.com>
2021-09-07 14:44:39 -0700

    r squared theory

--- machine-learning/practical-ml-tutorial/regression/bestFitLine.py
+++ machine-learning/practical-ml-tutorial/regression/bestFitLine.py
@@ -1,7 +1,7 @@
 from statistics import mean
+from matplotlib import style
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import style
 
 style.use("fivethirtyeight")
 
@@ -19,8 +19,20 @@ def best_fit_slope_and_intercept(xs, ys):
     return m, b
 
 
+# difference between regression line and actual ys
+def squared_error(ys_original, ys_line):
+    return sum((ys_line - ys_original) ** 2)
+
+
+# find r^2 value
+def coefficient_of_determination(ys_original, ys_line):
+    y_mean_line = [mean(ys_original) for y in ys_original]
+    squared_error_regr = squared_error(ys_original, ys_line)
+    squared_error_y_mean = squared_error(ys_original, y_mean_line)
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+
 m, b = best_fit_slope_and_intercept(xs, ys)
-print(m, b)
 
 # create regression line of y values based on all xs
 # equivalent to:
@@ -31,6 +43,12 @@ regression_line = [(m * x) + b for x in xs]
 # predict y where x = 8
 predict_x = 8
 predict_y = (m * predict_x) + b
+
+r_squared = coefficient_of_determination(ys, regression_line)
+
+print(f"m: {m}")
+print(f"b: {b}")
+print(f"R^2: {r_squared}")
 
 plt.scatter(xs, ys)
 plt.scatter(predict_x, predict_y, color="GREEN")
